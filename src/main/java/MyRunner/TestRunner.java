3f65272421c68b246e96176e6a0c8c7f13ac41a0
PARAG JAIN <87814011+paragjain15@users.noreply.github.com>
2023-11-23 22:42:35 +0530

    Added support for calling Dynamic App_ID

--- src/main/java/MyRunner/TestRunner.java
+++ src/main/java/MyRunner/TestRunner.java
@@ -19,62 +19,56 @@ import org.openqa.selenium.JavascriptExecutor;
 import java.net.MalformedURLException;
 
 @CucumberOptions(features = "src/main/java/Features/todo.feature", glue = {
-  "stepDefinitions" }, plugin = "json:target/cucumber-reports/CucumberTestReport.json")
+        "stepDefinitions"}, plugin = "json:target/cucumber-reports/CucumberTestReport.json")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
-  private TestNGCucumberRunner testNGCucumberRunner;
+    private TestNGCucumberRunner testNGCucumberRunner;
 
-  public static RemoteWebDriver connection;
+    public static RemoteWebDriver connection;
 
-  @BeforeClass(alwaysRun = true)
-  public void setUpCucumber() {
-    testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
-  }
+    @BeforeClass(alwaysRun = true)
+    public void setUpCucumber() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
 
-  @BeforeMethod(alwaysRun = true)
-  @Parameters({ "deviceName", "platformVersion", "platformName" })
-  public void setUpClass(String deviceName, String platformVersion, String platformName) throws Exception {
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"deviceName", "platformVersion", "platformName"})
+    public void setUpClass(String deviceName, String platformVersion, String platformName) throws Exception {
 
-    String username = System.getenv("LT_USERNAME") == null ?
-      "YOUR_LT_USERNAME" :
-      System.getenv("LT_USERNAME");      //Enter your LambdaTest username at the place of YOUR_LT_USERNAME
-    String accesskey = System.getenv("LT_ACCESS_KEY") == null ?
-      "YOUR_LT_ACCESSKEY" :
-      System.getenv("LT_ACCESS_KEY");     //Enter your LambdaTest accessKey at the place of YOUR_LT_ACCESSKEY
-    String app_id = System.getenv("LT_APP_ID") == null ?
-      "lt://proverbial-android" :
-      System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
-    String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
+        String username = System.getenv("LT_USERNAME") == null ? "YOUR_LT_USERNAME" : System.getenv("LT_USERNAME");      //Enter your LambdaTest username at the place of YOUR_LT_USERNAME
+        String accesskey = System.getenv("LT_ACCESS_KEY") == null ? "YOUR_LT_ACCESSKEY" : System.getenv("LT_ACCESS_KEY");     //Enter your LambdaTest accessKey at the place of YOUR_LT_ACCESSKEY
+        String app_id = System.getenv("LT_APP_ID") == null ? "lt://proverbial-android" : System.getenv("LT_APP_ID");      //Enter your LambdaTest App ID at the place of lt://proverbial-android
+        String grid_url = System.getenv("LT_GRID_URL") == null ? "mobile-hub.lambdatest.com" : System.getenv("LT_GRID_URL");
 
-    DesiredCapabilities capability = new DesiredCapabilities();
+        DesiredCapabilities capability = new DesiredCapabilities();
 
-    capability.setCapability("platformName", platformName);
-    capability.setCapability("deviceName", deviceName);
-    capability.setCapability("platformVersion", platformVersion);
+        capability.setCapability("platformName", platformName);
+        capability.setCapability("deviceName", deviceName);
+        capability.setCapability("platformVersion", platformVersion);
 
-    capability.setCapability("build", "LT-appium-java-cucumber");
-    capability.setCapability("test", "Test Parallel");
-    capability.setCapability("isRealMobile", true);
+        capability.setCapability("build", "LT-appium-java-cucumber");
+        capability.setCapability("test", "Test Parallel");
+        capability.setCapability("isRealMobile", true);
 
-    capability.setCapability("app", app_id);     //Enter the app url here
-    capability.setCapability("devicelog", true);
-    capability.setCapability("network", false);
-    capability.setCapability("video", true);
-    capability.setCapability("console", true);
-    capability.setCapability("visual", true);
+        capability.setCapability("app", app_id);     //Enter the app url here
+        capability.setCapability("devicelog", true);
+        capability.setCapability("network", false);
+        capability.setCapability("video", true);
+        capability.setCapability("console", true);
+        capability.setCapability("visual", true);
 
-    String gridURL = "https://" + username + ":" + accesskey + "@" + grid_url + "/wd/hub";
-    connection = new RemoteWebDriver(new URL(gridURL), capability);
-  }
+        String gridURL = "https://" + username + ":" + accesskey + "@" + grid_url + "/wd/hub";
+        connection = new RemoteWebDriver(new URL(gridURL), capability);
+    }
 
-  @DataProvider
-  public Object[][] features() {
-    return testNGCucumberRunner.provideScenarios();
-  }
+    @DataProvider
+    public Object[][] features() {
+        return testNGCucumberRunner.provideScenarios();
+    }
 
-  @AfterClass(alwaysRun = true)
-  public void tearDownClass() {
-    testNGCucumberRunner.finish();
-  }
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() {
+        testNGCucumberRunner.finish();
+    }
 }
